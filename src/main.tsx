27cbe7cbe8753b7b767a27aa206e0bddfcf10509
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-03-24 12:48:58 +0100

    filtercontext

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { FilterProvider } from "./components/FilterContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <FilterProvider>
+      <App />
+    </FilterProvider>
+  </React.StrictMode>,
+);
